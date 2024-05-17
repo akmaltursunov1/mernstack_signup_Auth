96631899c541a6be50ac3c51f85013b6fb10adc5
akmaltursunov1 <husanboyumarov07@gmail.com>
2024-05-17 18:30:08 +0500

    f

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,10 +2,14 @@ const express = require("express");
 const {
   registerUser,
   loginController,
+  alluser,
+  refreshToken,
 } = require("../controller/userController");
+const { isAuthenticated } = require("../middlware/auth");
 const router = express.Router();
 
-router.use("/register", registerUser);
-router.use("/login", loginController);
-
+router.post("/register", registerUser);
+router.post("/login", loginController);
+router.get("/users", isAuthenticated, alluser);
+router.post("/refresh", isAuthenticated, refreshToken, alluser);
 module.exports = router;
